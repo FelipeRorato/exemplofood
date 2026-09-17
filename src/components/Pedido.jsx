8b7f5efb73e09cdfe4c813852f92bc7c1e538765
Felipe <feliperoratocosta1@gmail.com>
2026-09-17 09:01:49 -0300

    feat: codificando e entendendo reduce

--- src/components/Pedido.jsx
+++ src/components/Pedido.jsx
@@ -12,7 +12,7 @@ const Pedido = () => {
 
     //hook - usestate - manipula o estado da variavel
     //exemplos vai gerenciar a lista de itens do cardapio
-    const [itens,setItems]=useState(cardapio);
+    const [items,setItems]=useState(cardapio);
     const [status, setStatus]=useState('');
     const [enviar,setEnviar]=useState(false);
 
@@ -25,13 +25,22 @@ const Pedido = () => {
             //Map: percorre a lista para criar um novo array sem modificar o original
             prev.map(item=>
                 //ternario: verifica se o item da iteração atual é o que deve ser alterado
-                //spread(...item) : adiciona o item a lista atual ou modifica
-                //Math.max - objeto que arante que a quantidade nunca seja menor que 0
+                //spread(...item): adiciona o item a lista atual ou modifica
+                //Math.max: objeto que arante que a quantidade nunca seja menor que 0
                 //item: retorna  item intacto caso o id não corresponda
                 item.id===id ? {...item,quantidade: Math.max(0,item.quantidade + valor)}: item
             )
         )
     }
+
+    //Filter: seleciona apenas os produtos disponiveis e do carrinho
+    const produtosDisponiveis = items.filter(item =>item.disponivel);
+    const carrinho = items.filter(item.quantidade >0)
+
+    //Reduce: calcula a soma dos itens (preco*quantidade) e adiciona a taxa de entrega
+    const subTotal = carrinho.reduce((ac, item)=>ac.item.preco * item.quantidade, 0)
+    const total = subTotal >0 ? subTotal + taxaEntrega: 0;
+    
 
 
   return (
